17414a941111dbf3807bc746fe34b46e8fe3c3f8
irineub <irineugmestre@gmail.com>
2025-08-02 02:39:41 -0400

    prod-fixes

--- src/app/services/api.service.ts
+++ src/app/services/api.service.ts
@@ -2,12 +2,13 @@ import { Injectable } from '@angular/core';
 import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 import { Observable, throwError } from 'rxjs';
 import { catchError } from 'rxjs/operators';
+import { environment } from '../../environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ApiService {
-  private baseUrl = 'http://localhost:8000/api';
+  private baseUrl = environment.apiUrl;
 
   constructor(private http: HttpClient) { }
 
---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,4 @@
+export const environment = {
+  production: true,
+  apiUrl: 'http://srv938431.hstgr.cloud:8000/api'
+}; 
